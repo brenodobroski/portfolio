caa0f0e798a152a90a9cd9f9ca47ea124b3ac79d
Breno Dobroski <brenodobroski@gmail.com>
2023-10-07 13:48:40 -0300

    initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.scss";
+import BtnAbout from "./components/btn-about/BtnAbout";
+
+const Home = () => {
+  return (
+    <div>
+      <BtnAbout />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Btn-about/BtnAbout.jsx
@@ -0,0 +1,11 @@
+import "./BtnAbout.scss";
+
+const BtnAbout = () => {
+  return (
+    <div>
+      <button>Me contate agora</button>
+    </div>
+  );
+};
+
+export default BtnAbout;
