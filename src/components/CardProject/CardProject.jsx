20ef9331e639b18b25b0417aa2ffe47bc6763a9f
Breno Dobroski <brenodobroski@gmail.com>
2023-10-07 22:26:14 -0300

    feat: save card project

--- src/components/CardProject/CardProject.jsx
+++ src/components/CardProject/CardProject.jsx
@@ -3,13 +3,26 @@ import "./CardProject.scss";
 const CardProject = () => {
   return (
     <div className="CardProject">
-      <h2 className="CardProject__title">Redesign Neon</h2>
-      <img src="./neon.svg" alt="" />
-      <p className="CardProject__p">
-        Tecnologias usadas: HTML/SASS/JS. O site foi completamente adaptado para
-        celular, tablets e computadores, além de utilizar js para criar modais e
-        menus nos botões
-      </p>
+      <div className="background-blur-project"></div>
+      <img
+        className="CardProject__circle-project"
+        src="./circle-project.svg"
+        alt=""
+      />
+      <div className="CardProject__text">
+        <h2 className="CardProject__title">Redesign Neon</h2>
+        <img className="CardProject__img" src="./neon.svg" alt="" />
+
+        <div className="background-blur-p">
+          <p className="CardProject__p">
+            Tecnologias usadas: HTML/SASS/JS. O site foi completamente adaptado
+            para celular, tablets e computadores, além de utilizar js para criar
+            modais e menus nos botões
+          </p>
+
+          <button className=""> Link para o github</button>
+        </div>
+      </div>
     </div>
   );
 };
